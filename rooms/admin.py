7198e56a21df1a671814f2070397a9e2cc93aa17
Peter, Han <secret_wings@naver.com>
2020-08-25 23:50:50 +0900

    6.2

--- rooms/admin.py
+++ rooms/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
+
+
 @admin.register(models.RoomType, models.Facility, models.Amenity, models.HouseRule)
 class ItemAdmin(admin.ModelAdmin):
 
@@ -15,6 +17,23 @@ class RoomAdmin(admin.ModelAdmin):
 
     """ Item Admin Definition """
 
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")},
+        ),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
+        ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths",)}),
+        (
+            "More About the Space",
+            {
+                "classes": ("collapse",),
+                "fields": ("amenities", "facilities", "house_rules"),
+            },
+        ),
+        ("Last Details", {"fields": ("host",)}),
+    )
+
     list_display = (
         "name",
         "country",
@@ -27,15 +46,32 @@ class RoomAdmin(admin.ModelAdmin):
         "check_in",
         "check_out",
         "instant_book",
+        "count_amenities",
     )
 
     list_filter = (
         "instant_book",
+        "host__superhost",
+        "host__gender",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
         "city",
         "country",
     )
 
-    search_fields = ("^city", "^host__username")
+    ordering = ("name", "price", "bedrooms")
+
+    search_fields = ("=city", "^host__username")
+
+    filter_horizontal = ("amenities", "facilities", "house_rules")
+
+    def count_amenities(self, obj):
+        print(obj.amenities.all())
+        return "Potato"
+
+    count_amenities.short_description = "hello"
 
 
 @admin.register(models.Photo)
