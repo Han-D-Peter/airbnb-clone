278422857a7c87ee3c766a7962582e1beb9627fa
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2020-09-08 17:38:40 +0900

    15.3

--- users/views.py
+++ users/views.py
@@ -11,7 +11,6 @@ class LoginView(FormView):
     template_name = "users/login.html"
     form_class = forms.LoginForm
     success_url = reverse_lazy("core:home")
-    initial = {"email": "itnicolasme@gmail.com"}
 
     def form_valid(self, form):
         email = form.cleaned_data.get("email")
@@ -22,18 +21,18 @@ class LoginView(FormView):
         return super().form_valid(form)
 
 
-"""def post(self, request):
-        form = forms.LoginForm(request.POST)
-        if form.is_valid():
-            email = form.cleaned_data.get("email")
-            password = form.cleaned_data.get("password")
-            user = authenticate(request, username=email, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect(reverse("core:home"))
-        return render(request, "users/login.html", {"form": form})"""
-
-
 def log_out(request):
     logout(request)
     return redirect(reverse("core:home"))
+
+
+class SignUpView(FormView):
+
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")
+    initial = {"first_name": "Nicoas", "last_name": "serr", "email": "itn@las.com"}
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
