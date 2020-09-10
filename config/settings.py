9328bfbae46197a1244a263c523fac10edb15d19
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2020-09-10 11:12:16 +0900

    email fail

--- config/settings.py
+++ config/settings.py
@@ -137,6 +137,7 @@ MEDIA_URL = "/media/"
 # Email Configuration
 
 EMAIL_HOST = "smtp.mailgun.org"
-EMAIL_PORT = "25"
+EMAIL_PORT = "587"
 EMAIL_HOST_USER = os.environ.get("MAILGUN_USERNAME")
 EMAIL_HOST_PASSWORD = os.environ.get("MAILGUN_PASSWORD")
+EMAIL_FROM = "sexy_guy@sandbox1adb32c8adce4d11aa9053c9ea1dc46f.mailgun.org"
