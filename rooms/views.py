cc0999eb8c0acf29a478adc122703a23d0891de9
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2020-09-14 17:45:28 +0900

    20.6

--- rooms/views.py
+++ rooms/views.py
@@ -9,7 +9,7 @@ class HomeView(ListView):
     """ HomeView Definition """
 
     model = models.Room
-    paginate_by = 10
+    paginate_by = 12
     paginate_orphans = 5
     ordering = "created"
     context_object_name = "rooms"
@@ -92,9 +92,11 @@ class SearchView(View):
 
                 rooms = paginator.get_page(page)
 
-                return render(request, "rooms/search.html", {"form": form, "rooms":rooms})
+                return render(
+                    request, "rooms/search.html", {"form": form, "rooms": rooms}
+                )
 
         else:
             form = forms.SearchForm()
-        
+
         return render(request, "rooms/search.html", {"form": form})
