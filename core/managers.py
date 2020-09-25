f2d840fa8786132e6417a480dfaf8f4e1c26f596
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2020-09-25 16:15:20 +0900

    24.15

--- core/managers.py
+++ core/managers.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 
-class CustomReservation(models.Manager):
+class CustomModelManager(models.Manager):
     def get_or_none(self, **kwargs):
         try:
-            return self.get(**kwargs)
+            return self.get(*kwargs)
         except self.model.DoesNotExist:
             return None
