ec02ecf90ec9264b585113414116ab7ef7f9fb7f
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2020-09-25 13:01:25 +0900

    24.13

--- reservations/models.py
+++ reservations/models.py
@@ -43,7 +43,7 @@ class Reservation(core_models.TimeStampedModel):
     room = models.ForeignKey(
         "rooms.Room", related_name="reservations", on_delete=models.CASCADE
     )
-    objects = managers.CustomReservationManager()
+    objects = managers.CustomReservation()
 
     def __str__(self):
         return f"{self.room} - {self.check_in}"
