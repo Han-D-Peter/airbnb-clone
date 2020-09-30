5c49624cbdd6fabde3eaa04bea9c24e237d231bd
Peter, Han <secret_wings@naver.com>
2020-09-30 11:55:21 +0900

    25.9 error

--- lists/views.py
+++ lists/views.py
@@ -5,10 +5,10 @@ from . import models
 
 
 def toggle_room(request, room_pk):
-    action = redirect.GET.get("action", None)
-    room = models.Room.objects.get_or_none(pk=room_pk)
+    action = request.GET.get("action", None)
+    room = room_models.Room.objects.get_or_none(pk=room_pk)
     if room is not None and action is not None:
-        the_list, created = models.List.objects.get_or_create(
+        the_list, _ = models.List.objects.get_or_create(
             user=request.user, name="My Favourites Houses"
         )
         if action == "add":
@@ -21,3 +21,4 @@ def toggle_room(request, room_pk):
 class SeeFavsView(TemplateView):
 
     template_name = "lists/list_detail.html"
+    
